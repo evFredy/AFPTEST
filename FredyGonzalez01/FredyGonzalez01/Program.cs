c463872ffc1c1f4f3f138bcbe22f8c0836ec4b14
Fredy Gonzalez <elements_e.v@hotmail.com>
2022-05-05 00:14:29 -0600

    Go

---
+++ FredyGonzalez01/FredyGonzalez01/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+var ToProcess = ExtractData.GetFiles();
+
+foreach (var item in ToProcess)
+{
+    var Result = ExtractData.GetDataFormFile(item);
+    Console.WriteLine(JsonConvert.SerializeObject(Result, Formatting.Indented));
+}
+
+public static class ExtractData
+{
+    public static string[] GetFiles()
+    {
+        string[] Files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.txt");
+        foreach (var file in Files)
+        {
+            Console.WriteLine(file);
+        }
+        return Files;
+    }
+
+    public static PersonalData GetDataFormFile(string FileName)
+    {
+        string text = File.ReadAllText(FileName);
+        string[] lines = text.Split(Environment.NewLine);
+        var Result = new PersonalData();
+        foreach (string line in lines)
+        {
+            var m = Regex.Match(line, @":");
+            if (m.Success)
+            {
+                var Value = line.Substring(m.Index + m.Length).Trim();
+                if (IsDui(Value))
+                {
+                    Result.DUI = Value;
+                }
+                else if (IsFecha(Value))
+                {
+                    Result.FechaNacimiento = DateTime.ParseExact(Value, "dd/MM/yyyy", null);
+                }
+                else if (IsNombre(Value))
+                {
+                    if (string.IsNullOrWhiteSpace(Result.Nombres))
+                    {
+                        Result.Nombres = Value;
+                    }
+                    else
+                    {
+                        Result.Apellidos = Value;
+                    }
+                }
+            }
+        }
+        return Result;
+    }
+
+    public static bool IsDui(string Value)
+    {
+        return Regex.IsMatch(Value, @"^\d+$");
+    }
+
+    public static bool IsNombre(string Value)
+    {
+        return Regex.IsMatch(Value, @"^[a-zA-Z ]+$");
+    }
+
+    public static bool IsFecha(string Value)
+    {
+        return Regex.IsMatch(Value, @"\d\d/\d\d/\d\d\d\d");
+    }
+}
+
+public class PersonalData
+{
+    public string Nombres { get; set; }
+
+    public string Apellidos { get; set; }
+
+    public string DUI { get; set; }
+
+    public DateTime FechaNacimiento { get; set; }
+}
---
+++ FredyGonzlez03/FredyGonzlez03/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+
+var Apis = new List<ApiData>();
+Apis.Add(new ApiData("San Salvador"));
+Apis.Add(new ApiData("Ahuachapan"));
+Apis.Add(new ApiData("Cojutepeque"));
+
+foreach (var item in Apis)
+{
+    var LatitudLongitud = await item.LatitudLongitud();
+    foreach (var Process in LatitudLongitud)
+    {
+        var lat = Process.FirstOrDefault(p => p.Key == "lat");
+        var lon = Process.FirstOrDefault(p => p.Key == "lon");
+        var ResultWeather = await item.Weather(lat.Value.ToString(), lon.Value.ToString());
+        var Result = ResultWeather.Where(p => p.Key == "wind" || p.Key == "main" || p.Key == "timezone");
+        await File.AppendAllTextAsync("Log.txt", "\n" + DateTime.Now.ToString() + " " + item.CityName + "\n");
+        await File.AppendAllTextAsync("Log.txt", JsonConvert.SerializeObject(Result, Formatting.Indented));
+    }
+}
+
+public class ApiData
+{
+    public ApiData(string cityName, string apiKey = "b003b168006b7a5e8e9e4781c4d37336", string countryCode = "503", string limit = "3")
+    {
+        ApiKey=apiKey;
+        CityName=cityName;
+        CountryCode=countryCode;
+        Limit=limit;
+    }
+
+    public string ApiKey { get; set; }
+
+    public string CityName { get; set; }
+
+    public string CountryCode { get; set; }
+
+    public string Limit { get; set; }
+
+    public async Task<IList<IDictionary<string, object>>> LatitudLongitud()
+    {
+        var webClient = new HttpClient();
+        var Uri = $"http://api.openweathermap.org/geo/1.0/direct?q={CityName},{CountryCode}&limit={Limit}&appid={ApiKey}";
+        var Response = await webClient.GetAsync(Uri).ConfigureAwait(false);
+        var jsonString = await Response.Content.ReadAsStringAsync();
+        var Result = JsonConvert.DeserializeObject<IList<IDictionary<string, object>>>(jsonString);
+        return Result ?? new List<IDictionary<string, object>>();
+    }
+
+    public async Task<IDictionary<string, object>> Weather(string Latitud, string Longitud)
+    {
+        var webClient = new HttpClient();
+        var Uri = $"https://api.openweathermap.org/data/2.5/weather?lat={Latitud}&lon={Longitud}&appid={ApiKey}";
+        var Response = await webClient.GetAsync(Uri).ConfigureAwait(false);
+        var jsonString = await Response.Content.ReadAsStringAsync();
+        var Result = JsonConvert.DeserializeObject<IDictionary<string, object>>(jsonString);
+        return Result ?? new Dictionary<string, object>();
+    }
+}
